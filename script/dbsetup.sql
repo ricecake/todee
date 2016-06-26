ad07ccf3c395ae616793280a7af555c9e003c1af
Sebastian Green-Husted <ricecake@tfm.nu>
2016-06-26 19:03:15 -0400

    Update dbsetup.sql

--- script/dbsetup.sql
+++ script/dbsetup.sql
@@ -5,7 +5,10 @@ BEGIN;
         username text not null
     );
     CREATE TABLE entries (
-        id serial primary key
+        id serial primary key,
+        date timestamp without timezone not null default now(),
+        calories integer not null,
+        weight integer not null
     );
 
 COMMIT;
