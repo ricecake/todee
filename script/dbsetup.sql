4a1de89c5a6d23aa15cd72c07b7fc8941853d54a
Sebastian Green-Husted <ricecake@tfm.nu>
2016-06-27 19:14:29 -0400

    Update dbsetup.sql

--- script/dbsetup.sql
+++ script/dbsetup.sql
@@ -5,23 +5,36 @@ BEGIN;
     );
     CREATE TABLE calorie_entry (
         id serial primary key,
-        entrant integer references todee_user(id),
+        entrant integer not null references todee_user(id),
         entry_time timestamp without time zone not null default now(),
         calories integer not null
     );
     CREATE TABLE weight_entry (
         id serial primary key,
-        entrant integer references todee_user(id),
+        entrant integer not null references todee_user(id),
         entry_time timestamp without time zone not null default now(),
         weight integer not null
     );
 
     create or replace view user_daily as
-        select u.id, date_trunc('day', c.entry_time), sum(c.calories), avg(w.weight)
+        select
+            u.id as entrant,
+            date_trunc('day', c.entry_time) as 'entry_day',
+            sum(c.calories) as 'total_calories',
+            avg(w.weight) as 'average_weight'
         from todee_user u
         left join calorie_entry c on c.entrant = u.id
         left join weight_entry  w on w.entrant = u.id
         where date_trunc('day', c.entry_time) = date_trunc('day', w.entry_time)
         group by u.id, date_trunc('day', c.entry_time);
+
+        create index on calorie_entry (entry_time);
+        create index on calorie_entry (entrant);
+        create index on calorie_entry (calories);
+
+        create index on weight_entry (entry_time);
+        create index on weight_entry (entrant);
+        create index on weight_entry (weight);
+
 
 COMMIT;
