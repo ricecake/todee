04a951d817b7ea91335e5b9d97bb2cb3bd0d402b
Sebastian Green-Husted <ricecake@tfm.nu>
2016-06-28 18:36:50 -0400

    Update dbsetup.sql

--- script/dbsetup.sql
+++ script/dbsetup.sql
@@ -21,10 +21,17 @@ BEGIN;
             u.id as entrant,
             date_trunc('day', c.entry_time) as entry_day,
             sum(c.calories) as total_calories,
-            avg(w.weight) as average_weight
+            w.weight as last_weight
         from todee_user u
         left join calorie_entry c on c.entrant = u.id
-        left join weight_entry  w on w.entrant = u.id
+        left join (
+            select
+                max(id) as max_id,
+                entrant
+            from weight_entry
+            group by entrant
+                             )  w_max on w_max.entrant = u.id
+        join weight_entry w on w.id = w_max.max_id
         where date_trunc('day', c.entry_time) = date_trunc('day', w.entry_time)
         group by u.id, date_trunc('day', c.entry_time);
 
